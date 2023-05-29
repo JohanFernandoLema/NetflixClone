bc25f94d0a7123662eb70e0119bacb05d44dad44
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-29 15:38:14 -0400

    question section finished

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 //traversing the dom
 //traverse ===> travel across or through
+
 const btns = document.querySelectorAll('.question-btn')
 
 btns.forEach((btn) => {
   btn.addEventListener('click', function (e) {
     const question = e.currentTarget.parentElement.parentElement
-
     question.classList.toggle('show-text')
   })
 })
