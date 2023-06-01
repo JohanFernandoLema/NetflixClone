735fa66ea5c2709800a095fafa721d0c36abbeb3
JohanFernandoLema <johan.13fernando@gmail.com>
2023-06-01 19:08:07 -0400

    nifty javascript methods used in order to make questions section collapsable

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question')
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn')
+  // console.log(btn)
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text')
+      }
+    })
+    question.classList.toggle('show-text')
+  })
+})
 
 //traversing the dom
 //traverse ===> travel across or through
 
-const btns = document.querySelectorAll('.question-btn')
+// const btns = document.querySelectorAll('.question-btn')
 
-btns.forEach((btn) => {
-  btn.addEventListener('click', function (e) {
-    const question = e.currentTarget.parentElement.parentElement
-    question.classList.toggle('show-text')
-  })
-})
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle('show-text')
+//   })
+// })
